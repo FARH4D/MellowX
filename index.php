fc3729dbc640dae40c8b998c8dfcf9b8a054a1ff
benz02r <64090995+benz02r@users.noreply.github.com>
2022-04-05 00:35:23 +0100

    New approach for log in
    Using a new approach for log in, changes being made so no longer using old approach

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+    
+    $_SESSION;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>MellowX</title>
+</head>
+<body>
+
+    <a href="logout.php">Logout</a>
+    <h1>MellowX index page</h1>
+
+    <br>
+    Hello, Username. #This is where we will put in username from database
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+</head>
+<body>
+
+    <style type="text/css">
+    	#text{
+
+    	height: 25px;
+    	border-radius: 5px;
+    	padding: 4px;
+    	border: solid thin #aaa;
+    }
+
+    #button{
+
+    	padding: 10px;
+    	width: 100px;
+    	color: white;
+    	background-color: lightblue;
+    	border: none;
+    }
+    
+    #box{
+
+    	background-color: grey;
+    	margin: auto;
+    	width: 300px;
+    	padding: 20px;
+    }
+
+     </style>
+
+     <div id="box">
+     	
+     	
+     </div>
+    
+</body>
+</html>
